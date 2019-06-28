c4964646161a930975f48110319649a4bba1e03f
Sanchit Sharma <sanchitormoney@gmail.com>
2019-06-28 17:51:54 +1000

    TOP5

--- Sanchit's Top 5/Sanchit's Top 5/Form1.Designer.vb
+++ Sanchit's Top 5/Sanchit's Top 5/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.lbl0 = New System.Windows.Forms.Label()
         Me.btn1 = New System.Windows.Forms.Button()
@@ -28,11 +28,6 @@ Partial Class Form1
         Me.btn3 = New System.Windows.Forms.Button()
         Me.btn4 = New System.Windows.Forms.Button()
         Me.btn5 = New System.Windows.Forms.Button()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
-        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
         Me.lbl1 = New System.Windows.Forms.Label()
         Me.lbl2 = New System.Windows.Forms.Label()
         Me.lbl3 = New System.Windows.Forms.Label()
@@ -41,11 +36,21 @@ Partial Class Form1
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lbl12 = New System.Windows.Forms.Label()
+        Me.lbl22 = New System.Windows.Forms.Label()
+        Me.lbl32 = New System.Windows.Forms.Label()
+        Me.lbl42 = New System.Windows.Forms.Label()
+        Me.lbl52 = New System.Windows.Forms.Label()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lbl0
@@ -124,115 +129,70 @@ Partial Class Form1
         Me.btn5.Text = "5"
         Me.btn5.UseVisualStyleBackColor = False
         '
-        'PictureBox1
-        '
-        Me.PictureBox1.Location = New System.Drawing.Point(88, 72)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(100, 48)
-        Me.PictureBox1.TabIndex = 6
-        Me.PictureBox1.TabStop = False
-        Me.PictureBox1.Visible = False
-        '
-        'PictureBox2
-        '
-        Me.PictureBox2.Location = New System.Drawing.Point(88, 152)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(100, 48)
-        Me.PictureBox2.TabIndex = 7
-        Me.PictureBox2.TabStop = False
-        Me.PictureBox2.Visible = False
-        '
-        'PictureBox3
-        '
-        Me.PictureBox3.Location = New System.Drawing.Point(88, 232)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(100, 48)
-        Me.PictureBox3.TabIndex = 8
-        Me.PictureBox3.TabStop = False
-        Me.PictureBox3.Visible = False
-        '
-        'PictureBox4
-        '
-        Me.PictureBox4.Location = New System.Drawing.Point(88, 312)
-        Me.PictureBox4.Name = "PictureBox4"
-        Me.PictureBox4.Size = New System.Drawing.Size(100, 48)
-        Me.PictureBox4.TabIndex = 9
-        Me.PictureBox4.TabStop = False
-        Me.PictureBox4.Visible = False
-        '
-        'PictureBox5
-        '
-        Me.PictureBox5.Location = New System.Drawing.Point(88, 392)
-        Me.PictureBox5.Name = "PictureBox5"
-        Me.PictureBox5.Size = New System.Drawing.Size(100, 48)
-        Me.PictureBox5.TabIndex = 10
-        Me.PictureBox5.TabStop = False
-        Me.PictureBox5.Visible = False
-        '
         'lbl1
         '
-        Me.lbl1.Enabled = False
         Me.lbl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl1.ForeColor = System.Drawing.SystemColors.ControlLightLight
         Me.lbl1.Location = New System.Drawing.Point(200, 72)
         Me.lbl1.Name = "lbl1"
-        Me.lbl1.Size = New System.Drawing.Size(320, 48)
+        Me.lbl1.Size = New System.Drawing.Size(184, 48)
         Me.lbl1.TabIndex = 11
         Me.lbl1.Text = "Mount Everest"
         Me.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl1.Visible = False
         '
         'lbl2
         '
-        Me.lbl2.Enabled = False
         Me.lbl2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl2.ForeColor = System.Drawing.SystemColors.ControlLightLight
         Me.lbl2.Location = New System.Drawing.Point(200, 152)
         Me.lbl2.Name = "lbl2"
-        Me.lbl2.Size = New System.Drawing.Size(320, 48)
+        Me.lbl2.Size = New System.Drawing.Size(184, 48)
         Me.lbl2.TabIndex = 12
         Me.lbl2.Text = "K2"
         Me.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl2.Visible = False
         '
         'lbl3
         '
-        Me.lbl3.Enabled = False
         Me.lbl3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl3.ForeColor = System.Drawing.SystemColors.ControlLightLight
         Me.lbl3.Location = New System.Drawing.Point(200, 232)
         Me.lbl3.Name = "lbl3"
-        Me.lbl3.Size = New System.Drawing.Size(320, 48)
+        Me.lbl3.Size = New System.Drawing.Size(184, 48)
         Me.lbl3.TabIndex = 13
         Me.lbl3.Text = "Kangchenjunga"
         Me.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl3.Visible = False
         '
         'lbl4
         '
-        Me.lbl4.Enabled = False
         Me.lbl4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl4.ForeColor = System.Drawing.SystemColors.ControlLightLight
         Me.lbl4.Location = New System.Drawing.Point(200, 312)
         Me.lbl4.Name = "lbl4"
-        Me.lbl4.Size = New System.Drawing.Size(320, 48)
+        Me.lbl4.Size = New System.Drawing.Size(184, 48)
         Me.lbl4.TabIndex = 14
         Me.lbl4.Text = "Lhotse"
         Me.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl4.Visible = False
         '
         'lbl5
         '
-        Me.lbl5.Enabled = False
         Me.lbl5.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.lbl5.ForeColor = System.Drawing.SystemColors.ControlLightLight
         Me.lbl5.Location = New System.Drawing.Point(200, 392)
         Me.lbl5.Name = "lbl5"
-        Me.lbl5.Size = New System.Drawing.Size(320, 48)
+        Me.lbl5.Size = New System.Drawing.Size(184, 48)
         Me.lbl5.TabIndex = 15
         Me.lbl5.Text = "Makalu"
         Me.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl5.Visible = False
         '
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(88, 448)
+        Me.Button1.Location = New System.Drawing.Point(88, 480)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(96, 32)
         Me.Button1.TabIndex = 16
@@ -242,29 +202,149 @@ Partial Class Form1
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(216, 448)
+        Me.Button2.Location = New System.Drawing.Point(216, 480)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(96, 32)
         Me.Button2.TabIndex = 17
-        Me.Button2.Text = "Autoplay"
+        Me.Button2.Text = "Show All"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
         Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.Location = New System.Drawing.Point(344, 448)
+        Me.Button3.Location = New System.Drawing.Point(344, 480)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(96, 32)
         Me.Button3.TabIndex = 18
         Me.Button3.Text = "Sources"
         Me.Button3.UseVisualStyleBackColor = True
         '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = Global.Sanchit_s_Top_5.My.Resources.Resources.Makalu
+        Me.PictureBox5.Location = New System.Drawing.Point(88, 392)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(100, 48)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 10
+        Me.PictureBox5.TabStop = False
+        Me.PictureBox5.Visible = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = Global.Sanchit_s_Top_5.My.Resources.Resources.Lhotse
+        Me.PictureBox4.Location = New System.Drawing.Point(88, 312)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(100, 48)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 9
+        Me.PictureBox4.TabStop = False
+        Me.PictureBox4.Visible = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.Sanchit_s_Top_5.My.Resources.Resources.Kangchenjunga
+        Me.PictureBox3.Location = New System.Drawing.Point(88, 232)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(100, 48)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 8
+        Me.PictureBox3.TabStop = False
+        Me.PictureBox3.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = Global.Sanchit_s_Top_5.My.Resources.Resources.K2
+        Me.PictureBox2.Location = New System.Drawing.Point(88, 152)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 48)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 7
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Sanchit_s_Top_5.My.Resources.Resources.mount_everest
+        Me.PictureBox1.Location = New System.Drawing.Point(88, 72)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 48)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
+        '
+        'lbl12
+        '
+        Me.lbl12.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl12.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl12.Location = New System.Drawing.Point(400, 72)
+        Me.lbl12.Name = "lbl12"
+        Me.lbl12.Size = New System.Drawing.Size(120, 48)
+        Me.lbl12.TabIndex = 19
+        Me.lbl12.Text = "8,848m"
+        Me.lbl12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl12.Visible = False
+        '
+        'lbl22
+        '
+        Me.lbl22.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl22.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl22.Location = New System.Drawing.Point(400, 152)
+        Me.lbl22.Name = "lbl22"
+        Me.lbl22.Size = New System.Drawing.Size(120, 48)
+        Me.lbl22.TabIndex = 20
+        Me.lbl22.Text = "8,611m"
+        Me.lbl22.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl22.Visible = False
+        '
+        'lbl32
+        '
+        Me.lbl32.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl32.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl32.Location = New System.Drawing.Point(400, 232)
+        Me.lbl32.Name = "lbl32"
+        Me.lbl32.Size = New System.Drawing.Size(120, 48)
+        Me.lbl32.TabIndex = 21
+        Me.lbl32.Text = "8,586m"
+        Me.lbl32.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl32.Visible = False
+        '
+        'lbl42
+        '
+        Me.lbl42.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl42.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl42.Location = New System.Drawing.Point(400, 312)
+        Me.lbl42.Name = "lbl42"
+        Me.lbl42.Size = New System.Drawing.Size(120, 48)
+        Me.lbl42.TabIndex = 22
+        Me.lbl42.Text = "8,516m"
+        Me.lbl42.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl42.Visible = False
+        '
+        'lbl52
+        '
+        Me.lbl52.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl52.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbl52.Location = New System.Drawing.Point(400, 392)
+        Me.lbl52.Name = "lbl52"
+        Me.lbl52.Size = New System.Drawing.Size(120, 48)
+        Me.lbl52.TabIndex = 23
+        Me.lbl52.Text = "8,485m"
+        Me.lbl52.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.lbl52.Visible = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(525, 489)
+        Me.ClientSize = New System.Drawing.Size(525, 541)
+        Me.Controls.Add(Me.lbl52)
+        Me.Controls.Add(Me.lbl42)
+        Me.Controls.Add(Me.lbl32)
+        Me.Controls.Add(Me.lbl22)
+        Me.Controls.Add(Me.lbl12)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
@@ -284,13 +364,14 @@ Partial Class Form1
         Me.Controls.Add(Me.btn2)
         Me.Controls.Add(Me.btn1)
         Me.Controls.Add(Me.lbl0)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Form1"
-        Me.Text = "Form1"
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "Top 5 Mountains (By Sanchit)"
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -315,6 +396,9 @@ Partial Class Form1
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Button3 As Button
-
-
+    Friend WithEvents lbl12 As Label
+    Friend WithEvents lbl22 As Label
+    Friend WithEvents lbl32 As Label
+    Friend WithEvents lbl42 As Label
+    Friend WithEvents lbl52 As Label
 End Class
--- Sanchit's Top 5/Sanchit's Top 5/Form1.vb
+++ Sanchit's Top 5/Sanchit's Top 5/Form1.vb
@@ -1,3 +1,76 @@
 ﻿Public Class Form1
 
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("Data Source: https://en.wikipedia.org/wiki/List_of_highest_mountains_on_Earth
+Images:
+https://en.wikipedia.org/wiki/Mount_Everest
+https://en.wikipedia.org/wiki/K2
+https://en.wikipedia.org/wiki/Kangchenjunga
+https://en.wikipedia.org/wiki/Lhotse
+https://en.wikipedia.org/wiki/Makalu")
+    End Sub
+
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        PictureBox1.Visible = True
+        lbl1.Visible = True
+        lbl12.Visible = True
+        btn2.Enabled = True
+    End Sub
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        PictureBox2.Visible = True
+        lbl2.Visible = True
+        lbl22.Visible = True
+        btn3.Enabled = True
+    End Sub
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        PictureBox3.Visible = True
+        lbl3.Visible = True
+        lbl32.Visible = True
+        btn4.Enabled = True
+    End Sub
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        PictureBox4.Visible = True
+        lbl4.Visible = True
+        lbl42.Visible = True
+        btn5.Enabled = True
+    End Sub
+    Private Sub Btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        PictureBox5.Visible = True
+        lbl5.Visible = True
+        lbl52.Visible = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        PictureBox1.Visible = True
+        lbl1.Visible = True
+        lbl12.Visible = True
+        btn2.Enabled = True
+
+        PictureBox2.Visible = True
+        lbl2.Visible = True
+        lbl22.Visible = True
+        btn3.Enabled = True
+
+        PictureBox3.Visible = True
+        lbl3.Visible = True
+        lbl32.Visible = True
+        btn4.Enabled = True
+
+        PictureBox4.Visible = True
+        lbl4.Visible = True
+        lbl42.Visible = True
+        btn5.Enabled = True
+
+        PictureBox5.Visible = True
+        lbl5.Visible = True
+        lbl52.Visible = True
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Application.Restart()
+    End Sub
+
 End Class
